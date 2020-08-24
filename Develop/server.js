a75e323534749ad958d52eb3acb53abb77b176ac
Moo Point <beachgal0105@gmail.com>
2020-08-23 17:51:03 -0700

    update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./app/config/connection');
+const sequelize = require('./config/connection');
 // DONE import sequelize connection
 
 const app = express();
