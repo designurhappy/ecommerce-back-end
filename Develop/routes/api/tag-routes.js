0bfd3d45abcf14e84c896325762a17ac5319f960
Moo Point <beachgal0105@gmail.com>
2020-08-28 00:01:11 -0700

    file corrections

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
     ],
   })
     .then((tags) => res.status(200).json(tags))
-    .catch((err) => res.status(404).json(err));
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
@@ -46,11 +46,13 @@ router.put('/:id', (req, res) => {
   // DONE update a tag's name by its `id` value
   Tag.update(req.body, {
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
   })
-    .then((tag) => res.status(200).json(tag))
-    .catch((err) => res.status(404).json(err));
+  .then((upTag) => {
+    res.json(upTag);
+  })
+  .catch((err) => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
